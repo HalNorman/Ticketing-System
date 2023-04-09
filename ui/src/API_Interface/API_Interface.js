ac7d8ca25e4e866d3b8c24f22d2d4b841ba823ff
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2023-04-08 18:55:12 -0700

    Added password verification to log in

--- ui/src/API_Interface/API_Interface.js
+++ ui/src/API_Interface/API_Interface.js
@@ -1,28 +1,28 @@
-// import axios from 'axios';
+ import axios from 'axios';
 
-// const AxiosConfigured = () => {
-//     // Indicate to the API that all requests for this app are AJAX
-//     axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const AxiosConfigured = () => {
+    // Indicate to the API that all requests for this app are AJAX
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-//     // Set the baseURL for all requests to the API domain instead of the current domain
-//     // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
-//     axios.defaults.baseURL = `http://localhost:8443/api/v1`;
-
-
-//     // Allow the browser to send cookies to the API domain (which include auth_token)
-//     axios.defaults.withCredentials = true;
+    // Set the baseURL for all requests to the API domain instead of the current domain
+    // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+    axios.defaults.baseURL = `http://localhost:8443/api/v1`;
 
 
-// //    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
-
-//     return axios;
-// };
+    // Allow the browser to send cookies to the API domain (which include auth_token)
+    axios.defaults.withCredentials = true;
 
 
-// const axiosAgent = AxiosConfigured();
+//    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
+
+    return axios;
+};
+
+
+const axiosAgent = AxiosConfigured();
 
 export default class APIInterface {
-/*
+
     async getUserInfo(user_id) {
         return axiosAgent.get(`login/${user_id}`)
             .then(userInfo => userInfo.data)
@@ -32,5 +32,14 @@ export default class APIInterface {
                     user: undefined
                  }));
     }
-*/
+
+    async getPasswordInfo(passwordInput) {
+        return axiosAgent.get(`login/password/${passwordInput}`)
+            .then(password => password.data)
+            .catch(error => (
+                {
+                    error,
+                    password: undefined
+                 }));
+    }
 }
