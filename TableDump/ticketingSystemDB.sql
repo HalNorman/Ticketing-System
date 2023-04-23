cbffb81bc580aa26d534fa8e8c2516d71c47c7a8
Ian-Hoole <129810670+Ian-Hoole@users.noreply.github.com>
2023-04-23 13:08:18 -0700

    Add files via upload
    Added a function to return tickets based on userID

--- TableDump/ticketingSystemDB.sql
+++ TableDump/ticketingSystemDB.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS `ticketingsystem`.`ticketfieldtag`;
 DROP TABLE IF EXISTS `ticketingsystem`.`fieldtag`;
 DROP TABLE IF EXISTS `ticketingsystem`.`ticket`;
 DROP TABLE IF EXISTS `ticketingsystem`.`user`;
+DROP FUNCTION IF EXISTS `ticketingsystem`.`getTicketsByUserID`;
 -- option to add ZEROFILL before NOT NULL
 DROP DATABASE IF EXISTS `ticketingsystem`;
 CREATE DATABASE `ticketingsystem`;
@@ -34,6 +35,13 @@ CREATE TABLE `ticketingsystem`.`user` (
     REFERENCES `ticketingsystem`.`user` (`userID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+
+-- CREATE TABLE `ticketingsystem`.`usermodticket` (
+-- 	`userID` INT UNSIGNED NOT NULL,
+-- 	`ticketID` INT UNSIGNED NOT NULL,
+--    `modified` DATE NOT NULL,
+--    PRIMARY KEY (`userID`)
+-- )
 
 
 CREATE TABLE `ticketingsystem`.`fieldtag` (
@@ -94,12 +102,32 @@ CREATE TABLE `ticketingsystem`.`templatefieldtag` (
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
+DELIMITER //
+CREATE FUNCTION `ticketingsystem`.`getTicketsByUserID`
+(
+	currTicketID INT UNSIGNED,
+    currUserID INT UNSIGNED,
+	gotUserID INT UNSIGNED
+) RETURNS INT UNSIGNED
+BEGIN
+	DECLARE gotUserRole INT;
+	SELECT `role` INTO gotUserRole FROM `ticketingsystem`.`user` WHERE userID = gotUserID;
+    IF gotUserID = currUserID
+		THEN RETURN currTicketID;
+	ELSEIF gotUserRole > 2
+		THEN RETURN NULL;
+	ELSE 
+		RETURN currTicketID;
+	END IF;
+END; //
+DELIMITER ;
+
 INSERT INTO `ticketingsystem`.`user` (`fName`, `lName`, `role`, `username`, `password`) VALUES ('setFirstName', 'setLastName', 'admin', 'admin', 'password');
 INSERT INTO `ticketingsystem`.`user` (`fName`, `lName`, `role`, `username`, `password`) VALUES ('Bob', 'McGee', 'employee', 'bmcgee', 'password');
 INSERT INTO `ticketingsystem`.`user` (`fName`, `lName`, `role`, `username`, `password`) VALUES ('Bill', 'McGoo', 'user', 'bmcgoo', 'password');
 
 INSERT INTO `ticketingsystem`.`ticket` (`userID`, `title`, `info`, `status`, `dateCreated`, `dateModified`, `dateCompleted`) VALUES (2, "Test Ticket", "This is ticket information", "active", "2020-01-21", "2020-01-21", null);
-INSERT INTO `ticketingsystem`.`ticket` (`userID`, `title`, `info`, `status`, `dateCreated`, `dateModified`, `dateCompleted`) VALUES (2, "Test Ticket2", "This is ticket information2", "complete", "2020-02-21", "2020-03-21", "2021-01-01");
+INSERT INTO `ticketingsystem`.`ticket` (`userID`, `title`, `info`, `status`, `dateCreated`, `dateModified`, `dateCompleted`) VALUES (3, "Test Ticket2", "This is ticket information2", "complete", "2020-02-21", "2020-03-21", "2021-01-01");
 
 INSERT INTO `ticketingsystem`.`fieldtag` (`field`, `tag`) VALUES ("Computer Type", "PC");
 INSERT INTO `ticketingsystem`.`fieldtag` (`field`, `tag`) VALUES ("Computer Type", "Mac");
@@ -128,7 +156,6 @@ SELECT A.`ticketID`, A.`title`, D.`fName`, D.`lName`, C.`field`, C.`tag`, A.`inf
 -- This is an example of a ticket template with 3 tags, title, and info on the template
 SELECT * FROM `ticketingsystem`.`template` A INNER JOIN `ticketingsystem`.`templatefieldtag` B ON A.`templateID` = B.`templateID` INNER JOIN `ticketingsystem`.`fieldtag` C ON B.`fieldtagID` = C.`fieldtagID` WHERE B.`templateID` = 1;
 
-SELECT * FROM  `ticketingsystem`.`template`;
-SELECT * FROM `ticketingsystem`.`fieldtag`;
-SELECT * FROM `ticketingsystem`.`ticket`;
-SELECT * FROM `ticketingsystem`.`user`;
+SELECT * FROM `ticketingsystem`.`ticket` A INNER JOIN (SELECT `ticketingsystem`.`getTicketsByUserID`(`ticketID`, `userID`, 2) as ticketID FROM `ticketingsystem`.`ticket`) B ON A.`ticketID` = B.`ticketID`;
+
+
