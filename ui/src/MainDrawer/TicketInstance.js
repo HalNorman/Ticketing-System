23f79c7b28155a9d73b710bc25fe0f18bc340ed4
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2023-04-26 21:40:37 -0700

    ticket instance only uses database now

--- ui/src/MainDrawer/TicketInstance.js
+++ ui/src/MainDrawer/TicketInstance.js
@@ -10,251 +10,153 @@ import {
 
 
 
-// const template =
-// {
-//     "TemplateTicket": [
-//       {
-//         "templateID": 1,
-//         "title": "this is a template",
-//         "info": "something useful",
-//         "fieldtagID": 1,
-//         "field": "Computer Type",
-//         "tag": "PC",
-//         "valid": 1
-//       },
-//       {
-//         "templateID": 1,
-//         "title": "this is a template",
-//         "info": "something useful",
-//         "fieldtagID": 4,
-//         "field": "Building",
-//         "tag": "Ives",
-//         "valid": 1
-//       },
-//       {
-//         "templateID": 1,
-//         "title": "this is a template",
-//         "info": "something useful",
-//         "fieldtagID": 6,
-//         "field": "Issue",
-//         "tag": "Liquid Spill",
-//         "valid": 1
-//       }
-//     ]
-// };
-
-// const fields = template.TemplateTicket.map((ticket) => ({
-//   fieldtagID: ticket.fieldtagID,
-//   field: ticket.field,
-//   tag: ticket.tag,
-// }));
-
-// const fieldTags = {
-//     fieldtags: [
-//       {
-//         fieldtagID: 1,
-//         field: "Computer Type",
-//         tags: [
-//           {
-//             tag: "PC",
-//           },
-//           {
-//             tag: "Mac",
-//           },
-//           {
-//             tag: "Linux",
-//           },
-//         ],
-//       },
-//       {
-//         fieldtagID: 4,
-//         field: "Building",
-//         tags: [
-//           {
-//             tag: "Ives",
-//           },
-//           {
-//             tag: "Tanna",
-//           },
-//           {
-//             tag: "Arin",
-//           },
-//           {
-//             tag: "Hale",
-//           },
-//           {
-//             tag: "Een",
-//           },
-//         ],
-//       },
-//       {
-//         fieldtagID: 6,
-//         field: "Issue",
-//         tags: [
-//           {
-//             tag: "Liquid Spill",
-//           },
-//           {
-//             tag: "Solid Spill",
-//           },
-//           {
-//             tag: "Gas Leak",
-//           },
-//         ],
-//       },
-//     ],
-//   };
-
 const Fields = (props) => {
-  console.log("ksbjhsg");
-  console.log("dfjkfdkjhfj ", props.ticket);
-  
+ 
 
-  const[fieldtags, setFieldTags] = useState([]);
-  const[ticket, setTicket] = useState([]);
+  const[fieldTags, setFieldTags] = useState([]);
+  const[allFieldTags, setAllFieldTags] = useState([]);
+  const[template, setTemplate] = useState([]);
+  const [fields, setFields] = useState([]);
 
   useEffect(() => {
     const api = new API();
 
-    async function getTickets() {
-        const routesJSONString = await  api.getTicketByID(props.ticket.ticketID);
-        console.log(`Tickets from the DB ${JSON.stringify(routesJSONString)}`);
-        setTicket(routesJSONString.data);
-    }
+
+    async function getTemplate() {
+      const routesJSONString = await  api.getTemplateFieldsByID(props.ticket.ticketID);
+      console.log(`Fields from the DB ${JSON.stringify(routesJSONString)}`);
+      setTemplate(routesJSONString.data);
+    
+  }
 
     async function getFieldTags() {
-      const routesJSONString = await  api.getAllFieldTags();
-      console.log(`Field Tags from the DB ${JSON.stringify(routesJSONString)}`);
-      setFieldTags(routesJSONString.data);
+      const routesJSONString = await  api.getAllFieldTags(props.ticket.ticketID);
+      console.log(`All Fields from the DB ${JSON.stringify(routesJSONString)}`);
+      setAllFieldTags(routesJSONString.data);
+    
   }
-    getTickets();
+    getTemplate();
     getFieldTags();
-    console.log("firsrdhg ", ticket);
 }, []);
 
 
-  const template =
-  {
-      "TemplateTicket": [
-        {
-          "templateID": 1,
-          "title": "hard coded title",
-          "info": "In Progress",
-          "fieldtagID": 1,
-          "field": "Computer Type",
-          "tag": "PC",
-          "valid": 1
-        },
-        {
-          "templateID": 1,
-          "title": "this is a template",
-          "info": "something useful",
-          "fieldtagID": 4,
-          "field": "Building",
-          "tag": "Ives",
-          "valid": 1
-        },
-        {
-          "templateID": 1,
-          "title": "this is a template",
-          "info": "something useful",
-          "fieldtagID": 6,
-          "field": "Issue",
-          "tag": "Liquid Spill",
-          "valid": 1
-        }
-      ]
-  };
+
+  useEffect(() => {
+    
+    setFields  (template.map((ticket) => ({
+      fieldtagID: ticket.fieldtagID,
+      field: ticket.field,
+      tag: ticket.tag,
+    })));
+   // console.log("jenfjfn ", template);
+
+    const api = new API();
+
+  
+
+
+  async function getFieldTags(fields) {
+    const routesJSONString = await api.getAllFieldTags();
+    console.log(`Field Tags from the DB ${JSON.stringify(routesJSONString)}`);
+  
+    // extract fieldtagID values from fields array
+    const fieldTagIDs = fields.map((field) => field.fieldtagID);
+  
+    // filter field tags by fieldTagID and templateID
+    const filteredFieldTags = routesJSONString.data.filter((fieldTag) =>
+      fieldTagIDs.includes(fieldTag.fieldtagID)// && fieldTag.templateID === templateID
+    );
+  
+    // extract tag values from filtered field tags
+    const tags = filteredFieldTags.map((fieldTag) => fieldTag.tag);
+  
+    // set tags in state
+    setFieldTags(filteredFieldTags);
+
+   
+    const newfieldTags = {
+      fieldtags: fields.map((field, index) => ({
+        fieldtagID: field.fieldtagID,
+        field: field.field,
+        tags: allFieldTags.filter(obj => obj.field === field.field).map(obj => obj.tag)
+      }))
+    };
+    setFieldTags(newfieldTags);
+   // setAllFieldTags(fields);
+
+    console.log("fields ", fields);
+    console.log("field tags ids", fieldTagIDs);
+    console.log("filtered field tags ", filteredFieldTags);
+    console.log("tags ", tags);
+    console.log("new field tags ", newfieldTags);
+    console.log("all field tags ", allFieldTags);
+  }
+ 
+    // console.log("fields: ", fields)
+    // console.log("template: ", template)
+
+   getFieldTags(fields)
+}, [template, allFieldTags]);
 
 
 
-const fields = template.TemplateTicket.map((ticket) => ({
-  fieldtagID: ticket.fieldtagID,
-  field: ticket.field,
-  tag: ticket.tag,
-}));
 
-const fieldTags = {
-    fieldtags: [
-      {
-        fieldtagID: 1,
-        field: "Computer Type",
-        tags: [
-          {
-            tag: "PC",
-          },
-          {
-            tag: "Mac",
-          },
-          {
-            tag: "Linux",
-          },
-        ],
-      },
-      {
-        fieldtagID: 4,
-        field: "Building",
-        tags: [
-          {
-            tag: "Ives",
-          },
-          {
-            tag: "Tanna",
-          },
-          {
-            tag: "Arin",
-          },
-          {
-            tag: "Hale",
-          },
-          {
-            tag: "Een",
-          },
-        ],
-      },
-      {
-        fieldtagID: 6,
-        field: "Issue",
-        tags: [
-          {
-            tag: "Liquid Spill",
-          },
-          {
-            tag: "Solid Spill",
-          },
-          {
-            tag: "Gas Leak",
-          },
-        ],
-      },
-    ],
-  };
-
-//const Fields = () => { //brought down const template, const fields, const fieldTags. This was originally outside of our Fields function
+  const title = props.ticket.title;
+  const info =  props.ticket.info;
 
 
 
-  const title = template.TemplateTicket[0].title;
-  const info =  template.TemplateTicket[0].info;
+ 
+  // const [selectedTags, setSelectedTags] = useState(
+  //   fields.reduce((obj, field) => {
+  //     obj[field.fieldtagID] = field.tag;
+  //     return obj;
+  //   }, {})
+  // );
 
-  const [selectedTags, setSelectedTags] = useState(
-    fields.reduce((obj, field) => {
-      obj[field.fieldtagID] = field.tag;
-      return obj;
-    }, {})
-  );
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  useEffect(() => {
+    setSelectedTags(fields);
+     console.log("selectedTags ", selectedTags);
+  
+  }, [fields]);
+
+
 
   const handleTagChange = (event, fieldtagID) => {
     const newSelectedTags = { ...selectedTags };
-    newSelectedTags[fieldtagID] = event.target.value;
+    newSelectedTags[fieldtagID].tag = event.target.value;
+
+    console.log("index ", fieldtagID, " value ",  event.target.value);
     setSelectedTags(newSelectedTags);
   };
 
+
   const getMenuItems = (fieldtagID) => {
+    if (fieldTags.length === 0) return null;
     const matchingField = fieldTags.fieldtags.find(
       (field) => field.fieldtagID === fieldtagID
     );
+    console.log("fieldtagss ", fieldTags);
+    console.log("fieldtag id ", fieldtagID);
+    console.log("match ", matchingField);
     if (!matchingField) return null;
+
+  
+    return (
+      matchingField && matchingField.tags ? (
+        matchingField.tags.map((tag) => (
+          <MenuItem key={tag} value={tag}>
+            {tag}
+          </MenuItem>
+        ))
+      ) : (
+        <MenuItem disabled>No tags found</MenuItem>
+      )
+    );
+
+
 
     return matchingField.tags.map((tag) => (
       <MenuItem key={tag.tag} value={tag.tag}>
@@ -266,10 +168,10 @@ const fieldTags = {
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    const selectedFields = fields.map((field) => ({
+    const selectedFields = fields.map((field, idx) => ({
       fieldtagID: field.fieldtagID,
       field: field.field,
-      tag: selectedTags[field.fieldtagID],
+      tag: selectedTags[idx],
     }));
 
     const formData = {
@@ -287,7 +189,7 @@ const fieldTags = {
       <Typography variant="h5">
         {info}
       </Typography>
-      {fields.map((field) => (
+      {fields.map((field, index) => (
         <Box
           key={field.fieldtagID}
           sx={{
@@ -302,8 +204,8 @@ const fieldTags = {
             {field.field}:
           </Typography>
           <Select
-            value={selectedTags[field.fieldtagID]}
-            onChange={(event) => handleTagChange(event, field.fieldtagID)}
+            value={selectedTags[index]?  selectedTags[index].tag: null}
+            onChange={(event) => handleTagChange(event, index)}
           >
             {getMenuItems(field.fieldtagID)}
           </Select>
